77cb50d66faa17f2103007b11489af2e376a2879
Bhupesh singh thalal <88537321+Bhupeshsinghthalal@users.noreply.github.com>
2023-07-13 18:09:40 +0530

    Add files via upload

---
+++ Olmpics Data Analysis/preprocessor.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def preprocess(df,region_df):
+    #filtering for summer olymics
+    df=df[df['Season']=='Summer']
+    
+    #merge with region_df
+    df=df.merge(region_df,on='NOC',how='left')
+    
+    #dropping duplicates
+    df.drop_duplicates(inplace=True)
+    
+    #one hot endoding medals
+    df=pd.concat([df,pd.get_dummies(df['Medal'])],axis=1)
+    return df
